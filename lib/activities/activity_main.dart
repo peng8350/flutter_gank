6f90697c454b7925782637979efd24853fe04851
Jpeng <peng8350@gmail.com>
2019-07-14 18:33:57 +0800

    add photo_acitivity

--- lib/activities/activity_main.dart
+++ lib/activities/activity_main.dart
@@ -6,6 +6,7 @@
 
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/physics.dart';
 import 'package:flutter_gank/App.dart';
--- lib/widget/cached_pic.dart
+++ lib/widget/cached_pic.dart
@@ -6,6 +6,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_gank/activities/activity_photo.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
@@ -38,14 +39,22 @@ class _CachedPicState extends State<CachedPic> {
     return new WillPopScope(
         child: new GestureDetector(
           child: new CachedNetworkImage(
-
-            placeholder: (c,s) => new Image.asset(widget.placeholder,fit: BoxFit.cover,),
+            placeholder: (c, s) => new Image.asset(
+              widget.placeholder,
+              fit: BoxFit.cover,
+            ),
             imageUrl: widget.url,
             fit: BoxFit.cover,
           ),
           onTap: () {
-            Overlay.of(context).insert(_scaleImg);
-            showScale = true;
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return GankPhotoActivity(urls: [
+                "https://pics5.baidu.com/feed/359b033b5bb5c9ea80848d91324096053bf3b3c8.jpeg?token=f4a0bcef6d4700e457fc058423758431&s=84A2E5B046610AA87A252C4C03009052",
+                "https://pics5.baidu.com/feed/359b033b5bb5c9ea80848d91324096053bf3b3c8.jpeg?token=f4a0bcef6d4700e457fc058423758431&s=84A2E5B046610AA87A252C4C03009052"
+              ], initIndex: 0);
+            }));
+//            Overlay.of(context).insert(_scaleImg);
+//            showScale = true;
           },
         ),
         onWillPop: _onWillPop);
@@ -55,23 +64,23 @@ class _CachedPicState extends State<CachedPic> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    _scaleImg = new OverlayEntry(builder: (context) {
-      return new GestureDetector(
-        child: new Container(
-          color: Colors.black,
-          padding: new EdgeInsets.only(top: 100.0,bottom: 100.0),
-          child: new PhotoView(
-            imageProvider: new NetworkImage(widget.url),
-
-            loadingChild: new Image.asset(widget.placeholder,width: double.infinity,height: double.infinity,fit: BoxFit.cover,),
-          ),
-        ),
-        onTap: () {
-          _scaleImg.remove();
-          showScale = false;
-        },
-      );
-    });
+//    _scaleImg = new OverlayEntry(builder: (context) {
+//      return new GestureDetector(
+//        child: new Container(
+//          color: Colors.black,
+//          padding: new EdgeInsets.only(top: 100.0,bottom: 100.0),
+//          child: new PhotoView(
+//            imageProvider: new NetworkImage(widget.url),
+//
+//            loadingChild: new Image.asset(widget.placeholder,width: double.infinity,height: double.infinity,fit: BoxFit.cover,),
+//          ),
+//        ),
+//        onTap: () {
+//          _scaleImg.remove();
+//          showScale = false;
+//        },
+//      );
+//    });
   }
 
   @override
